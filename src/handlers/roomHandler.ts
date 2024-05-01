5f23830ef7674f0b9d40f9f83d420755fd360e62
Sanket Singh <singhsanket143@gmail.com>
2024-05-01 22:11:32 +0530

    room creation logic added

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -7,15 +7,15 @@ const roomHandler = (socket: Socket) => {
         const roomId = UUIDv4(); // this will be our unique room id in which multiple connection will exchange data
         socket.join(roomId); // we will make the socket connection enter a new room
         socket.emit("room-created", { roomId }); // we will emit an event from server side that socket connection has been added to a room
-        console.log("Room created with id", roomId)
-    }
+        console.log("Room created with id", roomId);
+    };
 
     const joinedRoom = ({ roomId } : { roomId: string}) => {
         console.log("New user has joined room", roomId);
-    }
+    };
 
 
-    // When to call the above two function ? 
+    // When to call the above two function ?
 
 
     // We will call the above two function when the client will emit events top create room and join room
@@ -23,6 +23,6 @@ const roomHandler = (socket: Socket) => {
     socket.on("joined-room", joinedRoom);
 
 
-}
+};
 
 export default roomHandler;
