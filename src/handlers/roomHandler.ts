92f003700f4488c213904ab7f3a32008b845d1bd
Sanket Singh <singhsanket143@gmail.com>
2024-05-08 21:53:02 +0530

    Added ready event to emit new user joining for making calls on frontned

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -44,6 +44,13 @@ const roomHandler = (socket: Socket) => {
             console.log("added peer to room", rooms);
             socket.join(roomId); // make the user join the socket room
 
+            // whenever anyone joins the room
+
+            socket.on("ready", () => {
+                // from the frontend once someone joins the room we will emit a ready event
+                // then from our server we will emit an event to all the clients conn that a new peer has added
+                socket.to(roomId).emit("user-joined", {peerId}); 
+            })
 
             // below event is for logging purpose
             socket.emit("get-users", {
